d35aae7e1fdab1e1897d8255a4d2e6e37b868e20
soulone <ascf24032@gmail.com>
2019-10-31 12:21:49 -0500

    Commit -2

---
+++ Calculator.Droid/obj/Debug/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 69bb026477c0708b30b6593e863f889ec979ec39 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1215
+	/* entry-length */
+	.long	264
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 320761
+	.include	"typemap.jm.inc"
---
+++ Calculator.Droid/obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: e3c05ecc72205baf5f6a47b3a929d8233f1c74be */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1335
+	/* entry-length */
+	.long	264
+	/* value-offset */
+	.long	147
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 352441
+	.include	"typemap.mj.inc"
